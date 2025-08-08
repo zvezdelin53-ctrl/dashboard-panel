42c9d42ca7ce12bbef9f0d21f12acb32a671de1e
Zvezdelin <zvezdelin@example.com>
2025-08-09 00:53:10 +0200

    Начален комит – Dashboard

---
+++ main.tsx
@@ -0,0 +1,7 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => <h1>Здрасти, аз съм Джони – Шефа на Панела 🚀</h1>;
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
